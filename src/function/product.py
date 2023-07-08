897ad364f5f9e48366589660143c0f91af4584d0
Marcos Solari <msolari@infosis.tech>
2023-07-08 05:36:22 -0300

    Clean code and implementation functions

--- src/function/product.py
+++ src/function/product.py
@@ -1,6 +1,5 @@
 import psycopg2
 from src.sql.connection import connection
-from tkinter import Listbox
 
 name_db = connection.get_data().get('name_db')
 user_db = connection.get_data().get('user_db')
@@ -9,12 +8,18 @@ host_db = connection.get_data().get('host_db')
 port_db = connection.get_data().get('port_db')
 
 
-def save_new_product(name, category, price):
+def get_connection():
     conn = psycopg2.connect(dbname=f'{name_db}',
                             user=f'{user_db}',
                             password=f'{pass_db}',
                             host=f'{host_db}',
                             port=f'{port_db}', )
+
+    return conn
+
+
+def save_new_product(name, category, price):
+    conn = get_connection()
 
     # The cursor object allows queries to be made.
     cursor = conn.cursor()
@@ -27,15 +32,11 @@ def save_new_product(name, category, price):
 
 
 def get_products():
-    conn = psycopg2.connect(dbname=f'{name_db}',
-                            user=f'{user_db}',
-                            password=f'{pass_db}',
-                            host=f'{host_db}',
-                            port=f'{port_db}', )
+    conn = get_connection()
 
     cursor = conn.cursor()
 
-    query = '''SELECT name,category,price FROM product'''
+    query = '''SELECT * FROM product'''
 
     cursor.execute(query)
 
@@ -45,18 +46,13 @@ def get_products():
 
 
 def get_product_by_id(search_id):
-    conn = psycopg2.connect(dbname=f'{name_db}',
-                            user=f'{user_db}',
-                            password=f'{pass_db}',
-                            host=f'{host_db}',
-                            port=f'{port_db}', )
+    conn = get_connection()
 
     cursor = conn.cursor()
 
-    query = '''SELECT * FROM product WHERE id=%s'''
+    query = '''SELECT name, category, price FROM product WHERE id=%s'''
 
     cursor.execute(query, (search_id,))
-
 
     # Obtiene el primer dato
     data = cursor.fetchone()
@@ -67,3 +63,30 @@ def get_product_by_id(search_id):
 
     return data
 
+
+def delete_product(id):
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    query = '''DELETE FROM product WHERE id=%s'''
+
+    cursor.execute(query, (id,))
+
+    conn.commit()
+
+    conn.close()
+
+
+def update_product(id, name, category, price):
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    query = '''UPDATE product SET name=%s , category=%s, price=%s WHERE id=%s'''
+
+    cursor.execute(query, (name, category, price, id))
+
+    conn.commit()
+
+    conn.close()
--- src/product.py
+++ src/product.py
@@ -1,16 +1,26 @@
-from tkinter import Tk, Canvas, Frame, Label, Entry, Button, W, E, Listbox, END, ttk
-from function.product import save_new_product, get_products, get_product_by_id
+from tkinter import Tk, Frame, Label, Entry, Button, END, ttk, W, E, Listbox, font
+from function.product import save_new_product, get_product_by_id, get_products, delete_product, update_product
 
 root = Tk()
 root.title("Product crud")
+root.geometry("720x340")
+root.resizable(width=False, height=False)
 
-# Canvas
-canvas = Canvas(root, height=250, width=450)
-canvas.pack()
+bold_font = font.Font(weight="bold")
 
 # Frame
-frame = Frame()
-frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+frame = Frame(borderwidth=2, relief="sunken")
+frame.grid(row=0, column=0, pady=10, padx=10)
+
+# Frame
+frame_search = Frame()
+frame_search.grid(row=1, column=0)
+
+frame_product_result = Frame()
+frame_product_result.grid(row=1, column=1)
+
+lbl_title_product = Label(frame_product_result, text="Product", font=bold_font)
+lbl_title_product.grid(row=0, column=0)
 
 # Labels
 label = Label(frame, text="Add Product")
@@ -31,41 +41,30 @@ entry_category_product.grid(row=2, column=1)
 label = Label(frame, text="Price")
 label.grid(row=3, column=0)
 
+label_error = Label(frame, text="", foreground="red")
+label_error.grid(row=9, column=1)
+
 entry_price_product = Entry(frame)
 entry_price_product.grid(row=3, column=1)
 
-label_search = Label(frame, text="Search Data")
-label_search.grid(row=5, column=1)
-
-label = Label(frame, text="Search by ID: ")
-label.grid(row=5, column=0)
-
-entry_search_by_id = Entry(frame)
-entry_search_by_id.grid(row=5, column=1)
-
-button_search = Button(frame, text="Search", command=lambda: get_product_by_id(entry_search_by_id.get()))
-button_search.grid(row=5, column=2)
-
 # Button Add Product
 
-button = Button(frame, text="Add", command=lambda: add_and_display())
-button.grid(row=4, column=1)
-
-# ListBox Search by ID
-
-listbox_result_search = Listbox(frame, width=40, height=1)
-listbox_result_search.grid(row=9, columnspan=4, sticky=W + E)
+btn_add = Button(frame, text="Add", bg="#4EE172", command=lambda: add_and_display())
+btn_add.grid(row=6, column=1, sticky=W + E)
 
 # TreeView of products
 
-tree = ttk.Treeview()
+tree = ttk.Treeview(root)
 
 scrollbar = ttk.Scrollbar(root, orient="vertical", command=tree.yview)
+scrollbar.grid(row=0, column=2, pady=20, sticky="ns")
+
 tree.configure(yscrollcommand=scrollbar.set)
 
-tree["columns"] = ("Name", "Category", "Price")
+tree["columns"] = ("ID", "Name", "Category", "Price")
 
 tree.heading("#0", text="", anchor="w")
+tree.heading("ID", text="ID")
 tree.heading("Name", text="Name")
 tree.heading("Category", text="Category")
 tree.heading("Price", text="Price")
@@ -74,8 +73,67 @@ tree.column("#0", width=0, stretch=False)
 tree.column("Name", width=150)
 tree.column("Category", width=150)
 tree.column("Price", width=100)
-tree.pack(side="left", fill="both", expand=True)
-scrollbar.pack(side="right", fill="y")
+tree.column("ID", width=0, stretch=False)
+tree.grid(row=0, column=1, padx=5, pady=20)
+
+item_selected = None
+
+lbl_search = Label(frame_search, text="Search Product")
+lbl_search.grid(row=0, column=0)
+
+txt_search = Entry(frame_search)
+txt_search.grid(row=1, column=0)
+
+listbox = Listbox(frame_product_result, width=50, height=1)
+listbox.grid(row=3, column=0)
+
+# Button Delete
+btn_delete = Button(frame, text="Delete", bg="#C82222", command=lambda: delete_and_display(item_selected[0]))
+btn_delete.grid(row=7, column=1, sticky=W + E)
+
+btn_search = Button(frame_search, text="Search", bg="#ABABAB", command=lambda: get_product(txt_search.get()))
+btn_search.grid(row=1, column=1)
+
+for x in get_products():
+    tree.insert("", "end", values=x)
+
+
+def get_product(product_id):
+    product_found = get_product_by_id(int(product_id))
+
+    listbox.delete(0, END)
+
+    if product_found is not None:
+        listbox.config(bg="#C9CACC")
+        lbl_result_product = f'The product {product_found[0]} with category {product_found[1]} and price ${product_found[2]}'
+        listbox.insert(END, lbl_result_product)
+    else:
+        listbox.config(bg="white")
+        listbox.insert(END, "Not product found")
+
+
+def on_treeview_click(event):
+    global item_selected
+
+    item_id = tree.identify_row(event.y)
+    if item_id:
+        item_selected = tree.item(item_id)["values"]
+        modify_field_form(item_selected)
+    return item_selected
+
+
+def modify_field_form(data_field):
+    entry_name_product.delete(0, END)
+    entry_name_product.insert(0, data_field[1])
+    entry_category_product.delete(0, END)
+    entry_category_product.insert(0, data_field[2])
+    entry_price_product.delete(0, END)
+    entry_price_product.insert(0, data_field[3])
+
+    btn_add.config(text="Update", bg="#6971FF")
+
+
+tree.bind("<Button-1>", on_treeview_click)
 
 
 def clear_table():
@@ -84,21 +142,46 @@ def clear_table():
         tree.delete(child)
 
 
+def clear_fields():
+    entry_name_product.delete(0, END)
+    entry_category_product.delete(0, END)
+    entry_price_product.delete(0, END)
+
+
 def update_product_list():
     clear_table()
     for prod in get_products():
         tree.insert("", "end", values=prod)
 
 
-for x in get_products():
-    tree.insert("", "end", values=x)
-
-
 def add_and_display():
-    save_new_product(entry_name_product.get(),
-                     entry_category_product.get(),
-                     entry_price_product.get())
+    if entry_name_product.get() == "" or \
+            entry_price_product.get() == "" \
+            or entry_category_product.get() == "":
+        label_error.config(text="Field is required")
+    else:
+        if btn_add.cget("text") == "Add":
+            save_new_product(entry_name_product.get(),
+                             entry_category_product.get(),
+                             entry_price_product.get())
+            label_error.config(text="")
+        else:
+            update_product(item_selected[0],
+                           entry_name_product.get(),
+                           entry_category_product.get(),
+                           entry_price_product.get())
+            btn_add.config(text="Add", bg="#4EE172")
+            label_error.config(text="")
+
+    clear_fields()
     update_product_list()
+
+
+def delete_and_display(product_id):
+    delete_product(product_id)
+    update_product_list()
+    clear_fields()
+    btn_add.config(text="Add", bg="#4EE172")
 
 
 root.mainloop()
